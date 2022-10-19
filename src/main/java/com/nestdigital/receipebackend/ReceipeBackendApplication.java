3b4d3d5ab72ab1d68b527b56eb26a696f886c980
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-19 21:26:15 +0530

    new

---
+++ src/main/java/com/nestdigital/receipebackend/ReceipeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.receipebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceipeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/receipebackend/ReceipeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.receipebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
